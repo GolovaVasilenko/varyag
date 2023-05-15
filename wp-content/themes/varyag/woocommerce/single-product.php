0cf6f2547b8feacf85c337cb154d10252625509b
maestro <maestro@syneforge.com>
2023-05-15 12:49:30 +0300

    change design single coach page

--- wp-content/themes/varyag/woocommerce/single-product.php
+++ wp-content/themes/varyag/woocommerce/single-product.php
@@ -15,25 +15,32 @@
  * @version     1.6.4
  */
 
+function alx_varyag_scripts() {
+    wp_enqueue_style( 'varyag-single-product', get_template_directory_uri() . '/css/single_product.css', array(), _S_VERSION );
+}
+add_action( 'wp_enqueue_scripts', 'alx_varyag_scripts' );
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
 get_header('shop');?>
-<div class="container">
-<?php do_action( 'woocommerce_before_main_content' );?>
+<div class="inner-cart-page">
+    <div class="container">
+    <?php do_action( 'woocommerce_before_main_content' );?>
 
-<?php while ( have_posts() ) : ?>
-    <?php the_post(); ?>
+    <?php while ( have_posts() ) : ?>
+        <?php the_post(); ?>
 
-    <?php wc_get_template_part( 'content', 'single-product' ); ?>
+        <?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-<?php endwhile; // end of the loop. ?>
+    <?php endwhile; // end of the loop. ?>
 
-<?php
-do_action( 'woocommerce_after_main_content' );
-do_action( 'woocommerce_sidebar' );
-?>
+    <?php
+    do_action( 'woocommerce_after_main_content' );
+    do_action( 'woocommerce_sidebar' );
+    ?>
+    </div>
 </div>
 
 <?php
